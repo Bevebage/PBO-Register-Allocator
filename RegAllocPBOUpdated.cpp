3565103cb6ed7284f27eb471db4d1c7cbfb12866
Pierre Yan <pierreyan@college.harvard.edu>
2024-04-01 15:04:37 -0400

    thesis version

--- RegAllocPBOUpdated.cpp
+++ RegAllocPBOUpdated.cpp
@@ -92,6 +92,7 @@ class PBORegAllocUpdated : public MachineFunctionPass {
 
   std::vector<Register> VirtRegs;
   idx_t VirtRegCount;
+  std::map<Register, idx_t> VirtRegToId;
 
   std::map<SlotIndex, std::set<idx_t>> VIdsAtIdx;
 
@@ -109,7 +110,7 @@ class PBORegAllocUpdated : public MachineFunctionPass {
 
   std::map<MCPhysReg, MCPhysReg> DestToSrc;
   std::map<MCPhysReg, idx_t> DestToId;
-  std::map<MCPhysReg, MCPhysReg> Alias;
+  std::map<MCPhysReg, std::set<MCPhysReg>> Alias;
 
   std::vector<std::pair<MCPhysReg, idx_t>> SrcToZero;
   std::map<MCPhysReg, bool> Dests;
@@ -152,7 +153,7 @@ public:
 
 
   GRBVar genVar(void);
-  void genInstrRestrictions(const MachineInstr &, idx_t, bool);
+  void genInstrRestrictions(const MachineInstr &, idx_t, LiveInterval &, bool);
 
   void spillToStack(MachineBasicBlock &, MachineBasicBlock::instr_iterator &, Register, MCPhysReg);
   void loadViaStack(MachineBasicBlock &, MachineBasicBlock::instr_iterator &, Register, MCPhysReg);
@@ -164,6 +165,9 @@ public:
   void resetManager(void);
 
   int getStackSlot(Register);
+  MCPhysReg searchAliases(MCPhysReg, std::map<MCPhysReg, bool>);
+  MCPhysReg searchAliases(MCPhysReg, std::map<MCPhysReg, MCPhysReg>);
+  bool searchAliases(MCPhysReg, std::set<MCPhysReg>);
 
   static char ID;
 };
@@ -199,7 +203,7 @@ GRBVar PBORegAllocUpdated::genVar(void) {
   return Model->addVar(0.0, 1.0, 0.0, GRB_BINARY, "x" + std::to_string(++VarCount));
 }
 
-void PBORegAllocUpdated::genInstrRestrictions(const MachineInstr &Instr, idx_t VirtId, bool Spillable) {
+void PBORegAllocUpdated::genInstrRestrictions(const MachineInstr &Instr, idx_t VirtId, LiveInterval &LiveInterval, bool Spillable) {
   SlotIndex InstrIdx = SlotIndexes->getInstructionIndex(Instr).getBaseIndex();
 
   std::map<SlotIndex, std::set<idx_t>>::iterator IdSetIt = VIdsAtIdx.find(InstrIdx);
@@ -234,15 +238,27 @@ void PBORegAllocUpdated::genInstrRestrictions(const MachineInstr &Instr, idx_t V
     if (Operand.isRegMask()) {
       for (MCPhysReg PhysReg : PhysRegSet[VirtId]) {
         if (Operand.clobbersPhysReg(PhysReg)) {
-          Model->addConstr(PhysVars[VirtId][InstrIdx][PhysReg], GRB_EQUAL, 0, "Implicit Restriction");
+          Model->addConstr(PhysVars[VirtId][InstrIdx][PhysReg], GRB_EQUAL, 0, "Implicit Mask Restriction");
         }
       }
     }
-    
+
     if (Operand.isReg()) {
+      Register OperandReg = Operand.getReg();
+
+      if (OperandReg.isVirtual()) {
+        continue;
+      }
+
+      if (Operand.isDef() && !LiveInterval.isLiveAtIndexes(InstrIdx.getRegSlot())) {
+        continue;
+      }
+
+      LLVM_DEBUG(dbgs() << "Implicit: " << Operand.isDef() << " "; Instr.print(dbgs()));
+
       for (MCPhysReg PhysReg : PhysRegSet[VirtId]) {
-        if (TargetRegisterInfo->regsOverlap(Operand.getReg(), PhysReg)) {
-          Model->addConstr(PhysVars[VirtId][InstrIdx][PhysReg], GRB_EQUAL, 0, "Implicit Restriction");
+        if (TargetRegisterInfo->regsOverlap(OperandReg, PhysReg)) {
+          Model->addConstr(PhysVars[VirtId][InstrIdx][PhysReg], GRB_EQUAL, 0, "Implicit Reg Restriction");
         }
       }
     }
@@ -250,13 +266,17 @@ void PBORegAllocUpdated::genInstrRestrictions(const MachineInstr &Instr, idx_t V
 }
 
 void PBORegAllocUpdated::genStoreRestrictions(void) {
-  PhysRegSet = {};
-  PhysVars = {};
-  SpillVars = {};
+  PhysRegSet = std::vector<std::vector<MCPhysReg>>();
+  Alias = std::map<MCPhysReg, std::set<MCPhysReg>>();
+
+  PhysVars = std::vector<std::map<SlotIndex, std::map<MCPhysReg, GRBVar>>>();
+  SpillVars = std::vector<std::map<SlotIndex, GRBVar>>();
+  VirtRegToId = std::map<Register, idx_t>();
   VarCount = 0;
 
   for (idx_t VirtId = 0; VirtId < VirtRegCount; ++VirtId) {
     Register VirtReg = VirtRegs[VirtId];
+    VirtRegToId[VirtReg] = VirtId;
 
     const TargetRegisterClass &RegisterClass = *MachineRegisterInfo->getRegClass(VirtReg);
     LiveInterval &LiveInterval = LiveIntervals->getInterval(VirtReg);
@@ -275,6 +295,21 @@ void PBORegAllocUpdated::genStoreRestrictions(void) {
       }
 
       PhysRegSet[VirtId].push_back(*PhysRegIt);
+
+      // if (Alias.find(*PhysRegIt) != Alias.end()) {
+      //   continue;
+      // }
+
+      // Alias[*PhysRegIt] = std::set<MCPhysReg>();
+      // for (MCRegUnit RegUnit : TargetRegisterInfo->regunits(*PhysRegIt)) {
+      //   // for (MCRegUnitRootIterator RRI = MCRegUnitRootIterator(RegUnit, TargetRegisterInfo); RRI.isValid(); ++RRI) {
+      //   //   for (MCSuperRegIterator SI = MCSuperRegIterator(*RRI, TargetRegisterInfo); SI.isValid(); ++SI) {
+      //   //     *SI;
+      //   //     Alias[*PhysRegIt].insert(*SI);
+      //   //   }
+      //   // }
+        
+      // }
     }
 
     PhysVars.push_back(std::map<SlotIndex, std::map<MCPhysReg, GRBVar>>());
@@ -282,12 +317,12 @@ void PBORegAllocUpdated::genStoreRestrictions(void) {
 
     for (MachineRegisterInfo::def_instr_iterator DefIt = MachineRegisterInfo->def_instr_begin(VirtReg);
          DefIt != MachineRegisterInfo::def_instr_end(); ++DefIt) {
-      genInstrRestrictions(*DefIt, VirtId, false);
+      genInstrRestrictions(*DefIt, VirtId, LiveInterval, false);
     }
 
     for (MachineRegisterInfo::use_instr_iterator UseIt = MachineRegisterInfo->use_instr_begin(VirtReg);
          UseIt != MachineRegisterInfo::use_instr_end(); ++UseIt) {
-      genInstrRestrictions(*UseIt, VirtId, false);
+      genInstrRestrictions(*UseIt, VirtId, LiveInterval, false);
     }
 
     for (llvm::LiveInterval::iterator SegIt = LiveInterval.begin(); SegIt != LiveInterval.end(); ++SegIt) {
@@ -298,15 +333,13 @@ void PBORegAllocUpdated::genStoreRestrictions(void) {
           continue;
         }
 
-        genInstrRestrictions(*Instr, VirtId, true);
+        genInstrRestrictions(*Instr, VirtId, LiveInterval, true);
       }
     }
   }
 }
 
 void PBORegAllocUpdated::genClashRestrictions(void) {
-  Alias = std::map<MCPhysReg, MCPhysReg>();
-
   for (std::map<SlotIndex, std::set<idx_t>>::iterator EntryIt = VIdsAtIdx.begin(); EntryIt != VIdsAtIdx.end(); ++EntryIt) {
     for (std::set<idx_t>::iterator IdIt1 = EntryIt->second.begin(); IdIt1 != EntryIt->second.end(); ++IdIt1) {
       for (std::set<idx_t>::iterator IdIt2 = std::next(IdIt1, 1); IdIt2 != EntryIt->second.end(); ++IdIt2) {
@@ -316,11 +349,11 @@ void PBORegAllocUpdated::genClashRestrictions(void) {
               continue;
             }
 
-            Model->addConstr(PhysVars[*IdIt1][EntryIt->first][PhysReg1] + PhysVars[*IdIt2][EntryIt->first][PhysReg2], GRB_LESS_EQUAL, 1, "Clash Restriction");
-            
+            Model->addConstr(PhysVars[*IdIt1][EntryIt->first][PhysReg1] + PhysVars[*IdIt2][EntryIt->first][PhysReg2], GRB_LESS_EQUAL, 1, "Clash Restriction"); 
+
             if (PhysReg1 != PhysReg2) {
-              Alias[PhysReg1] = PhysReg2;
-              Alias[PhysReg2] = PhysReg1;
+              Alias[PhysReg1].insert(PhysReg2);
+              Alias[PhysReg2].insert(PhysReg1);
             }
           }
         }
@@ -335,11 +368,9 @@ void PBORegAllocUpdated::genBlockRestrictions(void) {
       continue;
     }
 
-    MachineInstrBundleIterator<MachineInstr> LastInstrIt = BasicBlock.getLastNonDebugInstr();
-    if (LastInstrIt->isTerminator()) {
-      while (LastInstrIt != BasicBlock.instr_begin() && std::prev(LastInstrIt)->isTerminator()) {
-        LastInstrIt--;
-      }
+    MachineInstrBundleIterator<MachineInstr> LastInstrIt = BasicBlock.getFirstTerminator();
+    if (LastInstrIt == BasicBlock.end()) {
+      LastInstrIt = BasicBlock.getLastNonDebugInstr();
     }
     SlotIndex LastInstrIdx = SlotIndexes->getInstructionIndex(*LastInstrIt).getBaseIndex();
 
@@ -430,6 +461,48 @@ void PBORegAllocUpdated::genObjectiveFunction(void) {
       }
 
       SlotIndex InstrIdx = SlotIndexes->getInstructionIndex(*InstrIt).getBaseIndex();
+
+      if (InstrIt->isCopy()) {
+        for (MachineOperand Def : InstrIt->defs()) {
+          if (!Def.isReg()) {
+            continue;
+          }
+
+          Register DefReg = Def.getReg();
+
+          for (MachineOperand Use : InstrIt->uses()) {
+            if (!Use.isReg()) {
+              continue;
+            }
+
+            Register UseReg = Use.getReg();
+
+            if (UseReg.isPhysical()) {
+              continue;
+            }
+
+            idx_t UseId = VirtRegToId[UseReg];
+
+            if (DefReg.isPhysical()) {
+              if (std::find(PhysRegSet[UseId].begin(), PhysRegSet[UseId].end(), DefReg.asMCReg()) != PhysRegSet[UseId].end()) {
+                ObjectiveExpr.addTerm(-ExpectedExecutions, PhysVars[UseId][InstrIdx][DefReg.asMCReg()]);
+              }
+            } else {
+              idx_t DefId = VirtRegToId[DefReg];
+
+              for (MCPhysReg DefPhysReg : PhysRegSet[DefId]) {
+                for (MCPhysReg UsePhysReg : PhysRegSet[UseId]) {
+                  if (DefPhysReg != UsePhysReg) {
+                    continue;
+                  }
+
+                  ObjectiveExpr.addTerm(-ExpectedExecutions, PhysVars[UseId][InstrIdx][UsePhysReg], PhysVars[DefId][InstrIdx][DefPhysReg]);
+                }
+              }
+            }
+          }
+        }
+      }
 
       std::map<idx_t, std::map<MCPhysReg, GRBVar>> CurInstrMap = {};
       std::map<idx_t, GRBVar> CurInstrSpill = {};
@@ -557,6 +630,49 @@ void PBORegAllocUpdated::substOperand(MachineBasicBlock::instr_iterator &Instr, 
   }
 }
 
+MCPhysReg PBORegAllocUpdated::searchAliases(MCPhysReg Src, std::map<MCPhysReg, bool> SearchSpace) {
+  if (Alias.find(Src) == Alias.end()) {
+    return 0;
+  }
+
+  for (MCPhysReg PhysReg : Alias[Src]) {
+    if (SearchSpace.find(PhysReg) != SearchSpace.end()) {
+      return PhysReg;
+    }
+  }
+  
+  return 0;
+}
+
+MCPhysReg PBORegAllocUpdated::searchAliases(MCPhysReg Src, std::map<MCPhysReg, MCPhysReg> SearchSpace) {
+  if (Alias.find(Src) == Alias.end()) {
+    return 0;
+  }
+
+  for (MCPhysReg PhysReg : Alias[Src]) {
+    if (SearchSpace.find(PhysReg) != SearchSpace.end()) {
+      return PhysReg;
+    }
+  }
+  
+  return 0;
+}
+
+
+bool PBORegAllocUpdated::searchAliases(MCPhysReg Src, std::set<MCPhysReg> SearchSpace) {
+  if (Alias.find(Src) == Alias.end()) {
+    return false;
+  }
+
+  for (MCPhysReg PhysReg : Alias[Src]) {
+    if (SearchSpace.find(PhysReg) != SearchSpace.end()) {
+      return true;
+    }
+  }
+  
+  return false;
+}
+
 void PBORegAllocUpdated::logMove(MCPhysReg Src, MCPhysReg Dest, idx_t VirtId) {
   LLVM_DEBUG(dbgs() << "Move from " << printReg(Src, TargetRegisterInfo) << " to " << printReg(Dest, TargetRegisterInfo) << "\n");
   if (Dest == 0) {
@@ -565,9 +681,16 @@ void PBORegAllocUpdated::logMove(MCPhysReg Src, MCPhysReg Dest, idx_t VirtId) {
 
     if (Dests.find(Src) != Dests.end()) {
       Dests[Src] = true;
-    } else if (Alias.find(Src) != Alias.end() && Dests.find(Alias[Src]) != Dests.end()) {
-      Dests[Alias[Src]] = true;
+    } else {
+      MCPhysReg Poss = searchAliases(Src, Dests);
+
+      if (Poss != 0) {
+        Dests[Poss] = true;
+      }
     }
+    // else if (Alias.find(Src) != Alias.end() && Dests.find(Alias[Src]) != Dests.end()) {
+    //   Dests[Alias[Src]] = true;
+    // }
     return;
   }
 
@@ -575,27 +698,43 @@ void PBORegAllocUpdated::logMove(MCPhysReg Src, MCPhysReg Dest, idx_t VirtId) {
   if (Dests.find(Src) != Dests.end()) {
     Dests[Src] = true;
     SrcOverlap = true;
-  } else if (Alias.find(Src) != Alias.end() && Dests.find(Alias[Src]) != Dests.end()) {
-    Dests[Alias[Src]] = true;
-    SrcOverlap = true;
+  } else {
+    MCPhysReg Poss = searchAliases(Src, Dests);
+
+    if (Poss != 0) {
+      Dests[Poss] = true;
+      SrcOverlap = true;
+    }
   }
+  // else if (Alias.find(Src) != Alias.end() && Dests.find(Alias[Src]) != Dests.end()) {
+  //   Dests[Alias[Src]] = true;
+  //   SrcOverlap = true;
+  // }
 
   bool DestOverlap = false;
-  if (Srcs.find(Dest) != Srcs.end() || (Alias.find(Dest) != Alias.end() && Srcs.find(Alias[Dest]) != Srcs.end())) {
+  if (Srcs.find(Dest) != Srcs.end() || searchAliases(Dest, Srcs)) {
     Dests[Dest] = true;
     DestOverlap = true;
   }
 
-  LLVM_DEBUG(dbgs() << "Src: " << SrcOverlap << " Dest: " << DestOverlap << "\n");
-  if (Alias.find(Src) != Alias.end()) {
-    LLVM_DEBUG(dbgs() << "SrcAlias: " << printReg(Alias[Src], TargetRegisterInfo) << "\n");
-  }
-  if (Alias.find(Dest) != Alias.end()) {
-    LLVM_DEBUG(dbgs() << "DestAlias: " << printReg(Alias[Dest], TargetRegisterInfo) << "\n");
-  }
-
   if (!DestOverlap) {
     Dests[Dest] = false;
+  }
+
+  if (Alias.find(Src) != Alias.end()) {
+    LLVM_DEBUG(dbgs() << "SrcAlias: ");
+    for (MCPhysReg PhysReg : Alias[Src]) {
+      LLVM_DEBUG(dbgs() << printReg(PhysReg, TargetRegisterInfo) << " ");
+    }
+    LLVM_DEBUG(dbgs() << "\n");
+  }
+
+  if (Alias.find(Dest) != Alias.end()) {
+    LLVM_DEBUG(dbgs() << "DestAlias: ");
+    for (MCPhysReg PhysReg : Alias[Dest]) {
+      LLVM_DEBUG(dbgs() << printReg(PhysReg, TargetRegisterInfo) << " ");
+    }
+    LLVM_DEBUG(dbgs() << "\n");
   }
 
   if (SrcOverlap && DestOverlap) {
@@ -606,21 +745,35 @@ void PBORegAllocUpdated::logMove(MCPhysReg Src, MCPhysReg Dest, idx_t VirtId) {
       if (DestToSrc.find(Traveler) != DestToSrc.end()) {
         Lagger = Traveler;
         Traveler = DestToSrc[Traveler];
-      } else if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
-        Lagger = Alias[Traveler];
-        Traveler = DestToSrc[Alias[Traveler]];
       } else {
-        break;
+        MCPhysReg Poss = searchAliases(Traveler, DestToSrc);
+
+        if (Poss != 0) {
+          Lagger = Poss;
+          Traveler = DestToSrc[Poss];
+        } else {
+          break;
+        }
       }
+      // else if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
+      //   Lagger = Alias[Traveler];
+      //   Traveler = DestToSrc[Alias[Traveler]];
+      // } else {
+      //   break;
+      // }
     }
 
     if (Traveler == Dest) {
       Dests[Dest] = false;
     }
-    if (Alias.find(Dest) != Alias.end() && Traveler == Alias[Dest]) {
+    if (searchAliases(Dest, std::set{Traveler})) {
       Dests[Dest] = false;
       DestToId[Traveler] = DestToId[Lagger];
     }
+    // if (Alias.find(Dest) != Alias.end() && Traveler == Alias[Dest]) {
+    //   Dests[Dest] = false;
+    //   DestToId[Traveler] = DestToId[Lagger];
+    // }
   }
 
   DestToSrc[Dest] = Src;
@@ -663,11 +816,19 @@ void PBORegAllocUpdated::addMoveInstr(MachineBasicBlock &BasicBlock, MachineBasi
     MCPhysReg Traveler = SrcIdPair.first, SrcReg;
     while (true) {
       if (DestToSrc.find(Traveler) == DestToSrc.end()) {
-        if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
-          Traveler = Alias[Traveler];
+        MCPhysReg Poss = searchAliases(Traveler, DestToSrc);
+
+        if (Poss != 0) {
+          Traveler = Poss;
         } else {
           break;
         }
+
+        // if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
+        //   Traveler = Alias[Traveler];
+        // } else {
+        //   break;
+        // }
       }
 
       SrcReg = DestToSrc[Traveler];
@@ -708,9 +869,12 @@ void PBORegAllocUpdated::addMoveInstr(MachineBasicBlock &BasicBlock, MachineBasi
       Traveler = SrcReg;
 
       if (DestToSrc.find(Traveler) == DestToSrc.end()) {
-        if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
+        MCPhysReg Poss = searchAliases(Traveler, DestToSrc);
+        
+        if (Poss != 0) {
+        // if (Alias.find(Traveler) != Alias.end() && DestToSrc.find(Alias[Traveler]) != DestToSrc.end()) {
           Stack.push_back(Traveler);
-          Traveler = Alias[Traveler];
+          Traveler = Poss;
 
           if (Traveler == Start.first) {
             Cycle = true;
@@ -727,7 +891,8 @@ void PBORegAllocUpdated::addMoveInstr(MachineBasicBlock &BasicBlock, MachineBasi
       spillToStack(BasicBlock, InsertInstrIt, VirtRegs[DestToId[Start.first]], Start.first);
 
       for (idx_t Idx = 0; Idx < Stack.size() - 1; ++Idx) {
-        if (Alias.find(Stack[Idx]) != Alias.end() && Alias[Stack[Idx]] == Stack[Idx+1]) {
+        // if (Alias.find(Stack[Idx]) != Alias.end() && Alias[Stack[Idx]] == Stack[Idx+1]) {
+        if (searchAliases(Stack[Idx], std::set{Stack[Idx+1]})) {
           continue;
         }
         moveRegToReg(BasicBlock, InsertInstrIt, Stack[Idx+1], Stack[Idx]);
@@ -737,7 +902,8 @@ void PBORegAllocUpdated::addMoveInstr(MachineBasicBlock &BasicBlock, MachineBasi
       loadViaStack(BasicBlock, InsertInstrIt, VirtRegs[DestToId[LastReg]], LastReg);
     } else {
       for (idx_t Idx = 0; Idx < Stack.size() - 1; ++Idx) {
-        if (Alias.find(Stack[Idx]) != Alias.end() && Alias[Stack[Idx]] == Stack[Idx+1]) {
+        // if (Alias.find(Stack[Idx]) != Alias.end() && Alias[Stack[Idx]] == Stack[Idx+1]) {
+        if (searchAliases(Stack[Idx], std::set{Stack[Idx+1]})) {
           continue;
         }
         moveRegToReg(BasicBlock, InsertInstrIt, Stack[Idx+1], Stack[Idx]);
@@ -852,28 +1018,23 @@ bool PBORegAllocUpdated::runOnMachineFunction(llvm::MachineFunction &MFunction) 
     StackCache[VirtReg] = -1;
   }
 
-
   ObjectiveExpr = GRBQuadExpr();
 
   GRBModel Solver = GRBModel(Env);
   Model = &Solver;
   Model->getEnv().set(GRB_DoubleParam_TimeLimit, 1200);
 
-
   genStoreRestrictions();
   genClashRestrictions();
   genBlockRestrictions();
   genTermsRestrictions();
 
-
   genObjectiveFunction();
   Model->setObjective(ObjectiveExpr);
-
 
   readPBOSolution();
   addBlockLiveIns();
   assignRegisters();
-
 
   for (Register VirtReg : VirtRegs) {
     LiveIntervals->removeInterval(VirtReg);
